8027904bb0ed4ae25013f3c19947334fbe473140
OnionPowder01 <autonatione36@gmail.com>
2022-11-28 16:47:41 +0200

    render all the data with reusable code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-
-import Card from './components/Card';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
+import Card from "./components/Card";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Data from "./components/Data"
+import React from 'react';
 
 export default function App() {
+  const cardsData = Data.map(everyCard => {
+    return <Card
+    img={everyCard.coverImg}
+    rating={everyCard.stats.rating}
+    reviewCount={everyCard.stats.reviewCount}
+    location={everyCard.location}
+    title={everyCard.title}
+    price={everyCard.price}
+    />
+  })
+
   return (
     <div className="big-container">
       <Navbar />
       <Hero />
-      <Card />
+      {cardsData}
     </div>
   );
 }
+
+
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,18 @@
 import React from "react";
-import katie from "../images/katie-zaferes.png";
 import star from "../images/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img src={katie} alt="katie" className="card--image" />
+      <img src={props.img} alt="card-post" className="card--image" />
       <div className="card--stats">
         <img src={star} alt="star" className="card--star" />
-        <span>5.0 </span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
-        <p className="card--para">Life lessons with Katie Zaferes</p>
-        <p className="card--para"><span className="bold">From $136</span> / person</p>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">  {props.location} </span>
+        <p className="card--para">{props.title}</p>
+        <p className="card--para"><span className="bold">From {props.price}$</span> / person</p>
       </div>
     </div>
   );
 }
-<span className="bold">From $136</span>
